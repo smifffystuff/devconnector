19fcb6e93f5b81ba255d777322ffcb4a8b8d0181
Martin Smith <martin.smifff@gmail.com>
2018-07-17 11:37:38 +0100

    Adding posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,33 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route   GET /api/posts/test
-// @desc    Test posts route
+const Post = require('../../models/Post');
+
+// @route   GET api/posts/test
+// @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({
   msg: 'Posts Works'
 }));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save()
+      .then(post => res.json(post));
+  });
 
 module.exports = router;
